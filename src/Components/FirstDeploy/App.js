3db9ccb03272deebdcd0913cb3eb865a46a979ba
Kay Suparx <nitt0x0@gmail.com>
2020-09-14 08:52:11 +0900

    0.0-open Repo

---
+++ src/Components/FirstDeploy/App.js
@@ -0,0 +1,52 @@
+/*
+* 누구든지 하는 리액트 #5 본격적인 리액트 코드 작성하기
+* http://bit.ly/2GB0F9O
+*
+**/
+
+
+// Component 폴더가 따로 정의되있지 않다면, {Component} 가 필요하다.
+import React, {Component} from 'react';
+
+import logo from '../../static/image/reactLogo.svg'
+import firebase from '../../static/image/fireBaseLogo.png'
+import '../../static/css/RollingReact.css';
+
+
+
+
+class App extends Component {
+  render() {
+    // 랜더매서드 안에서 변수선언 가능 = let 수시변경
+    // css를 JSX문법으로 바꾸는 방법 = 아래참조
+    const pageTitle = 'Hello~ REACT!'
+    const sayHello = 'This is the first deploy to ...';
+
+    let stylePop = {
+      paddingTop: '0px',
+      fontSize: '52px',
+      fontWeight: 'bold',
+      color : '#62ec12',
+      backgroundColor: '#f51183',
+    };
+
+    return (
+      // div 밖에서 코맨드 문법은 젓가락!
+
+      <div className={App}>
+        {/* div 안에서 코맨드 문법은 멀티라인 형식을 쓴다.
+          * <div/>  ... JSX 문법은 꼭 Pair tag로 닫는다. */}
+        <header className='App-header'>
+
+          <p style={stylePop}>{pageTitle}</p>
+          <img src={logo} className='App-logo' alt='reactLogo'/>
+
+          <div>{sayHello}</div>
+          <img className='img' src={firebase} width='220' alt='fireBaseLogo'/>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
